f6cffb2f8c187f963e9a2f4b0f6450934fec47fb
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-10 18:46:40 +0900

    Initial Commit

---
+++ OpenWeatherMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherMVVM
+//
+//  Created by 강석호 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
