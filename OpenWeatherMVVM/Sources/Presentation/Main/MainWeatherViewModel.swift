4bf418c3fc714960edb4b5e211c4c0e863ba622d
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-12 13:08:51 +0900

    [Feat] Weather description 데이터 추가

--- OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewModel.swift
+++ OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewModel.swift
@@ -11,23 +11,36 @@ class MainWeatherViewModel {
     
     var inputViewDidLoadTrigger: Observable<Void?> = Observable(nil)
     
-    var outputWeatherUpdate: Observable<(cityName: String, currentTemp: String)> = Observable(("", ""))
+    var outputWeatherUpdate: Observable<(cityName: String, currentTemp: String, descriptionName: String)> = Observable(("", "", ""))
+    
+    private let locationManager = LocationManager()
     
     init() {
         inputViewDidLoadTrigger.bind { _ in
-            self.fetchWeather()
+            self.requestLocation()
         }
     }
     
-    func fetchWeather() {
-        WeatherAPI.shared.fetchWeatherData(latitude: 37.74913611, longitude: 128.8784972) {  result in
+    private func requestLocation() {
+        locationManager.fetchLocation { [weak self] coordinate, error in
+            if let coordinate = coordinate {
+                self?.fetchWeather(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            } else if let error = error {
+                print("Error fetching location: \(error)")
+                self?.outputWeatherUpdate.value = ("Location Error", "N/A", "N/A")
+            }
+        }
+    }
+    
+    func fetchWeather(latitude: Double, longitude: Double) {
+        WeatherAPI.shared.fetchWeatherData(latitude: latitude, longitude: longitude) { result in
             switch result {
             case .success(let weatherData):
                 let celsiusTemp = self.convertCelsius(kelvin: weatherData.main.temp ?? 0.0)
                 let formattedTemp = String(format: "%.1f°C", celsiusTemp)
-                self.outputWeatherUpdate.value = (cityName: weatherData.name, currentTemp: formattedTemp)
+                self.outputWeatherUpdate.value = (cityName: weatherData.name, currentTemp: formattedTemp, descriptionName: weatherData.weather.first?.description ?? "")
             case .failure:
-                self.outputWeatherUpdate.value = ("Error", "N/A")
+                self.outputWeatherUpdate.value = ("Error", "N/A", "N/A")
             }
         }
     }
