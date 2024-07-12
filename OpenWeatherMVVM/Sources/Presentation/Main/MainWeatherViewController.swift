1e1d6a82afd758d16fcc9bc8a2954701e341d6ec
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-12 13:27:08 +0900

    [Fix] ScrollView로 UI 수정

--- OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewController.swift
+++ OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class MainWeatherViewController: BaseViewController {
     
+    let scrollView = UIScrollView()
+    let mainWatherView = MainWeatherView()
     let viewModel = MainWeatherViewModel()
-    
-    override func loadView() {
-        view = MainWeatherView()
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +19,33 @@ class MainWeatherViewController: BaseViewController {
         viewModel.inputViewDidLoadTrigger.value = ()
     }
     
+    override func configureHierarchy() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(mainWatherView)
+    }
+    
+    override func configureView() {
+        scrollView.backgroundColor = .black
+    }
+    
+    override func configureConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        mainWatherView.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.top)
+            make.width.equalTo(scrollView.snp.width)
+            make.height.equalTo(180)
+        }
+    }
+    
     func bindData() {
-        if let mainWeatherView = view as? MainWeatherView {
-            viewModel.outputWeatherUpdate.bind { weatherUpdate in
-                mainWeatherView.cityName.text = weatherUpdate.cityName
-                mainWeatherView.currentTemp.text = weatherUpdate.currentTemp
-            }
+        viewModel.outputWeatherUpdate.bind { weatherUpdate in
+            self.mainWatherView.cityName.text = weatherUpdate.cityName
+            self.mainWatherView.currentTemp.text = weatherUpdate.currentTemp
+            self.mainWatherView.descriptionName.text = weatherUpdate.descriptionName
+            
+            print("==========\(weatherUpdate.descriptionName)")
         }
     }
 }
