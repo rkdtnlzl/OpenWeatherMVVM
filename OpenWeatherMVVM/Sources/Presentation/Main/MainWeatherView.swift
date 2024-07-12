e417ee86b19e884429974eb75e5d3914d46dfb96
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-12 23:48:50 +0900

    [Feat] 최대최소온도 UI 추가

--- OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherView.swift
+++ OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherView.swift
@@ -13,11 +13,13 @@ class MainWeatherView: BaseView {
     let cityName = UILabel()
     let currentTemp = UILabel()
     let descriptionName = UILabel()
+    let minMaxTemp = UILabel()
     
     override func configureHierarchy() {
         addSubview(cityName)
         addSubview(currentTemp)
         addSubview(descriptionName)
+        addSubview(minMaxTemp)
     }
     
     override func configureView() {
@@ -35,6 +37,11 @@ class MainWeatherView: BaseView {
         descriptionName.textColor = .white
         descriptionName.font = .systemFont(ofSize: 25)
         descriptionName.textAlignment = .center
+        
+        minMaxTemp.text = "loading.."
+        minMaxTemp.textColor = .white
+        minMaxTemp.font = .systemFont(ofSize: 22)
+        minMaxTemp.textAlignment = .center
     }
     
     override func configureLayout() {
@@ -48,6 +55,10 @@ class MainWeatherView: BaseView {
         }
         descriptionName.snp.makeConstraints { make in
             make.top.equalTo(currentTemp.snp.bottom).offset(5)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        minMaxTemp.snp.makeConstraints { make in
+            make.top.equalTo(descriptionName.snp.bottom).offset(5)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
         }
     }
