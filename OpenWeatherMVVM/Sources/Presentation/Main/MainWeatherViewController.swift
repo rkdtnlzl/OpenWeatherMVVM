c23669bd2fa8405d811742973277fbc4fc61684a
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-13 17:26:41 +0900

    [Feat] 3시간 간격 일기예보 수평스크롤 collectionView 추가

--- OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewController.swift
+++ OpenWeatherMVVM/Sources/Presentation/Main/MainWeatherViewController.swift
@@ -12,6 +12,23 @@ class MainWeatherViewController: BaseViewController {
     
     let scrollView = UIScrollView()
     let mainWatherView = MainWeatherView()
+    lazy var threeHoursCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 16
+        layout.itemSize = CGSize(width: 80, height: 120)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .clear
+        collectionView.layer.cornerRadius = 10
+        collectionView.layer.borderColor = UIColor.lightGray.cgColor
+        collectionView.layer.borderWidth = 0.5
+        collectionView.register(ThreeHoursCollectionViewCell.self, forCellWithReuseIdentifier: ThreeHoursCollectionViewCell.id)
+        return collectionView
+    }()
+    
     let viewModel = MainWeatherViewModel()
     let locationManager = CLLocationManager()
     
@@ -30,6 +47,7 @@ class MainWeatherViewController: BaseViewController {
     override func configureHierarchy() {
         view.addSubview(scrollView)
         scrollView.addSubview(mainWatherView)
+        scrollView.addSubview(threeHoursCollectionView)
     }
     
     override func configureView() {
@@ -45,6 +63,13 @@ class MainWeatherViewController: BaseViewController {
             make.width.equalTo(scrollView.snp.width)
             make.height.equalTo(180)
         }
+        threeHoursCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(mainWatherView.snp.bottom).offset(20)
+            make.width.equalTo(scrollView.snp.width)
+            make.height.equalTo(120)
+        }
+    }
+    
     }
     
     func bindData() {
@@ -57,6 +82,17 @@ class MainWeatherViewController: BaseViewController {
     }
 }
 
+extension MainWeatherViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.outputThreeHoursWeather.value.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return cell
+    }
+    
+}
 extension MainWeatherViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
