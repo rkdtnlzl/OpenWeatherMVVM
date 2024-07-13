e2d48efb594596b86d5e94b5166e50a088c478c6
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-13 19:19:43 +0900

    [Feat] 5일 간의 일기예보 tableViewCell 생성

---
+++ OpenWeatherMVVM/Sources/Presentation/Main/FiveDaysTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  FiveDaysTableViewCell.swift
+//  OpenWeatherMVVM
+//
+//  Created by 강석호 on 7/13/24.
+//
+
+import UIKit
+import SnapKit
+
+class FiveDaysTableViewCell: BaseTableViewCell {
+    
+    static let id = "FiveDaysTableViewCell"
+    
+    let dateLabel = UILabel()
+    let weatherIcon = UIImageView()
+    let minTempLabel = UILabel()
+    let maxTempLabel = UILabel()
+    let stackView = UIStackView()
+    
+    override func configureHierarchy() {
+        stackView.addArrangedSubview(dateLabel)
+        stackView.addArrangedSubview(weatherIcon)
+        stackView.addArrangedSubview(minTempLabel)
+        stackView.addArrangedSubview(maxTempLabel)
+        addSubview(stackView)
+    }
+    
+    override func configureView() {
+        dateLabel.text = "오늘"
+        dateLabel.font = .systemFont(ofSize: 20)
+        dateLabel.textColor = .white
+        
+        weatherIcon.contentMode = .scaleAspectFit
+        weatherIcon.image = UIImage(systemName: "star")
+        
+        minTempLabel.text = "최저: 33도"
+        minTempLabel.font = .systemFont(ofSize: 20)
+        minTempLabel.textColor = .gray
+        
+        maxTempLabel.text = "최고: 33도"
+        maxTempLabel.font = .systemFont(ofSize: 20)
+        maxTempLabel.textColor = .white
+        
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+    }
+    
+    override func configureLayout() {
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(15)
+        }
+    }
+}
