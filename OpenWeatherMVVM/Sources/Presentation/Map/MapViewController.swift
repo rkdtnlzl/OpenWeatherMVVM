bd1cafa2454a595da04c6200caa1b1a4dab7ab8b
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-14 16:56:48 +0900

    [Feat] MapViewController 생성

---
+++ OpenWeatherMVVM/Sources/Presentation/Map/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  OpenWeatherMVVM
+//
+//  Created by 강석호 on 7/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class MapViewController: BaseViewController {
+    
+}
