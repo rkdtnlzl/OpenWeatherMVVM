97ee7514e21c7e612a81500eafab043215e2e6a5
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-14 23:33:16 +0900

    [Fix] MapVC -> MapSearchVC 이름 변경

---
+++ OpenWeatherMVVM/Sources/Presentation/Map/MapSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapSearchViewController.swift
+//  OpenWeatherMVVM
+//
+//  Created by 강석호 on 7/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class MapSearchViewController: BaseViewController {
+    
+    let cityTitle = UILabel()
+    let searchBar = UISearchBar()
+    let tableView = UITableView()
+    
+    
+    
+}
