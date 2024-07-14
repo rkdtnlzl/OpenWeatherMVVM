280b220479473b63fe04ef8958d034dae066d3b8
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-15 00:27:08 +0900

    [Feat] 도시 검색 ViewModel 생성

---
+++ OpenWeatherMVVM/Sources/Presentation/Map/CitySearchViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  CitySearchViewModel.swift
+//  OpenWeatherMVVM
+//
+//  Created by 강석호 on 7/14/24.
+//
+
+import Foundation
+
+class CitySearchViewModel {
+    
+    var inputSearchText: Observable<String> = Observable("")
+    var outputFilteredCities: Observable<[City]> = Observable([])
+    
+    private var cities: [City] = []
+    
+    init() {
+        loadCities()
+        inputSearchText.bind { searchText in
+            self.filterCities(with: searchText)
+        }
+    }
+    
+    private func loadCities() {
+        if let loadedCities = JSONParser.loadCities(from: "CityList") {
+            cities = loadedCities
+            outputFilteredCities.value = cities
+        }
+    }
+    
+    private func filterCities(with searchText: String) {
+        if searchText.isEmpty {
+            outputFilteredCities.value = cities
+        } else {
+            outputFilteredCities.value = cities.filter { city in
+                city.name.range(of: searchText, options: .caseInsensitive) != nil
+            }
+        }
+    }
+}
