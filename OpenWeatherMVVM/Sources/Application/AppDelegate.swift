dba801d391f36ebda71156db9c91ea1f5a359f6d
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-15 15:48:21 +0900

    [Feat] UINavigationBar appearance Scroll 배경색 추가

--- OpenWeatherMVVM/Sources/Application/AppDelegate.swift
+++ OpenWeatherMVVM/Sources/Application/AppDelegate.swift
@@ -11,7 +11,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .black
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
         return true
     }
 
@@ -28,7 +31,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
